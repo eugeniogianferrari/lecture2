e5c0db33e409f378fd5fdbcf7facb61d43b82a48
eugeniogianferrari <eugi2799@gmail.com>
2020-11-02 19:50:58 +0100

    flask sessions

--- notes_session/application.py
+++ notes_session/application.py
@@ -7,12 +7,13 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-notes = []
-
 @app.route("/", methods = ["GET","POST"])
 def index():
-    if request.method == "POST":            #if the user adds a new note
+    if session.get("notes") is None:                    
+        session["notes"] = []                           #create a "session" that lets differents users' data be independent from one another
+
+    if request.method == "POST":                        #if the user adds a new note
         note = request.form.get("note")
-        notes.append(note)                  #add the new note
+        session["notes"].append(note)                   #add the new note
     
-    return render_template("index.html", notes = notes)
+    return render_template("index.html", notes = session["notes"])
